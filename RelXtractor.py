dde6d329b473ad795abae8f9ed1bba3d5cc0b66e
Anish Krishna Vallapuram <akvallapuram@connect.ust.hk>
2019-02-10 19:07:18 +0800

    Added dossier feature

--- RelXtractor.py
+++ RelXtractor.py
@@ -3,19 +3,17 @@
     written by Anish Krishna Vallapuram
     09 Feb, 2019
 """
-import sys
 from newspaper import Article
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import sent_tokenize as sentence
 from textblob import TextBlob
 from itertools import groupby, combinations
-import pandas as pd
 from string import punctuation
 
 
 # reads sources.txt and returns urls
 def url_from_sources():
-    print('reading URLs from "sources.txt"')
+    print('reading SOURCEs from "sources.txt"')
     all_urls = []
     sources = open('sources.txt', 'r').read()
     sources = sources.split('\n')
@@ -30,7 +28,7 @@ def url_from_sources():
 
 # read the news sources
 def read_news(all_urls):
-    articles = {'Authors': [], 'Date': [], 'URL': [], 'TEXT': []}
+    articles = {'Authors': [], 'Date': [], 'SOURCE': [], 'TEXT': []}
     for url in all_urls:
         print('reading news from:\n', url)
         article = Article(url)
@@ -42,7 +40,7 @@ def read_news(all_urls):
             articles['Date'].append(date)
         else:
             articles['Date'].append(article.publish_date.strftime('%d/%m/%Y'))
-        articles['URL'].append(url)
+        articles['SOURCE'].append(url)
         # weird work-around
         article_ascii = article.text.encode('ascii', 'ignore')
         text = article_ascii.decode('utf-8').replace('\n\n', ' ')
@@ -53,7 +51,7 @@ def read_news(all_urls):
 # groups the entity tags from STANFORD NER
 def grouptags(tags, ignore="O", join=" "):
     for c, g in groupby(tags, lambda t: t[1]):
-        if ignore is None or c != ignore:
+        if ignore is None or c not in ignore:
             if join is None:
                 entity = [e for e, _ in g]
             else:
@@ -69,16 +67,13 @@ def strip_punct(s):
 # returns all the co-occurences in a give text
 def get_relations(articles):
     relations = {'NODE1': [], 'NODE2': [], 'TYPE': [], 'DATE': [],
-                 'SOURCE': [], 'URL': [], 'CONTEXT': []}
-    gz_path = 'stanford-ner-2018-10-16/classifiers/english.all.3class.distsim.crf.ser.gz'
+                 'SOURCEINTEXT': [], 'SOURCE': [], 'CONTEXT': []}
+    gz_path = "stanford-ner-2018-10-16/classifiers/english.all.3class.distsim.crf.ser.gz"
     jar_path = 'stanford-ner-2018-10-16/stanford-ner.jar'
-    if sys.platform == 'win32':
-        gz_path = "stanford-ner-2018-10-16\\classifiers\\english.all.3class.distsim.crf.ser.gz"
-        jar_path = 'stanford-ner-2018-10-16\\stanford-ner.jar'
     st = StanfordNERTagger(gz_path, jar_path, encoding='utf-8')
     for text in range(len(articles['TEXT'])):
         print('Analysing for sentiment and relations in:\n',
-              articles['URL'][text])
+              articles['SOURCE'][text])
         sentences = sentence(articles['TEXT'][text])
         for sent in sentences:
             # sentiment analysis
@@ -102,8 +97,9 @@ def get_relations(articles):
                 relations['NODE2'].append(strip_punct(r[1][1]))
                 relations['TYPE'].append(sentiment)
                 relations['DATE'].append(articles['Date'][text])
-                relations['SOURCE'].append(','.join(articles['Authors'][text]))
-                relations['URL'].append(articles['URL'][text])
+                relations['SOURCEINTEXT'].append(','.join(
+                                                 articles['Authors'][text]))
+                relations['SOURCE'].append(articles['SOURCE'][text])
                 relations['CONTEXT'].append(sent)
     print('Finished Analysing all news sources.')
     return relations
@@ -112,38 +108,20 @@ def get_relations(articles):
 # returns a list of all actors
 def get_actors(relation_data):
     print("Collecting Actors")
-    actors = {'NODE1': [], 'SOURCE': [], 'DATE': [],
-              'URL': []}
+    actors = {'NODE1': [], 'SOURCEINTEXT': [], 'DATE': [],
+              'SOURCE': []}
     for r in range(len(relation_data['NODE1'])):
-        source = relation_data['SOURCE'][r]
+        source = relation_data['SOURCEINTEXT'][r]
         date = relation_data['DATE'][r]
-        url = relation_data['URL'][r]
+        url = relation_data['SOURCE'][r]
         if relation_data['NODE1'][r] not in actors['NODE1']:
             actors['NODE1'].append(relation_data['NODE1'][r])
-            actors['SOURCE'].append(source)
+            actors['SOURCEINTEXT'].append(source)
             actors['DATE'].append(date)
-            actors['URL'].append(url)
+            actors['SOURCE'].append(url)
         if relation_data['NODE2'][r] not in actors['NODE1']:
             actors['NODE1'].append(relation_data['NODE2'][r])
-            actors['SOURCE'].append(source)
+            actors['SOURCEINTEXT'].append(source)
             actors['DATE'].append(date)
-            actors['URL'].append(url)
+            actors['SOURCE'].append(url)
     return actors
-
-
-def main():
-    all_urls = url_from_sources()
-    articles = read_news(all_urls)
-    relations = get_relations(articles)
-    actors = get_actors(relations)
-    print('Saving relation data to "connections.csv"')
-    df = pd.DataFrame(data=relations, columns=relations.keys())
-    df.to_csv('connections.csv', sep=',')
-    print('Saving actors data to "actors.csv"')
-    df1 = pd.DataFrame(data=actors, columns=actors.keys())
-    df1.to_csv('actors.csv', sep=',')
-    print("Finished Saving. Done!")
-
-
-if __name__ == '__main__':
-    main()
---
+++ main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import RelXTractor
+import read_dossier
+import sys
+
+
+# two features: news sites or dossier
+def use_sources():
+    all_urls = RelXTractor.url_from_sources()
+    articles = RelXTractor.read_news(all_urls)
+    return articles
+
+
+def use_dossier():
+    articles = read_dossier.read_dossier()
+    return articles
+
+
+def main():
+    articles = ''
+    if 'dossier' in sys.argv:
+        articles = use_dossier()
+    elif 'sources' in sys.argv:
+        articles = use_sources()
+    else:
+        print("ERROR: no source given. Choose from: 'dossier' and 'sources'")
+        return
+    relations = RelXTractor.get_relations(articles)
+    actors = RelXTractor.get_actors(relations)
+    print('Saving relation data to "connections.csv"')
+    df = pd.DataFrame(data=relations, columns=relations.keys())
+    df.to_csv('connections.csv', sep=',')
+    print('Saving actors data to "actors.csv"')
+    df1 = pd.DataFrame(data=actors, columns=actors.keys())
+    df1.to_csv('actors.csv', sep=',')
+    print("Finished Saving. Done!")
+
+
+if __name__ == '__main__':
+    main()
