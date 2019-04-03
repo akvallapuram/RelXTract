318db153a97e7a697f7fdf1a99852237e45f4871
Anish Krishna Vallapuram <akvallapuram@connect.ust.hk>
2019-04-03 11:37:45 +0800

    NER model try

---
+++ ner/ner.py
@@ -0,0 +1,15 @@
+import logging
+from pathlib import Path
+import sys
+
+import tensorflow as tf
+
+
+# Setup logging
+Path('results').mkdir(exist_ok=True)
+tf.logging.set_verbosity(logging.INFO)
+handlers = [
+    logging.FileHandler('results/main.log'),
+    logging.StreamHandler(sys.stdout)
+]
+logging.getLogger('tensorflow').handlers = handlers
